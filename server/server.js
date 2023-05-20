aed020f58b2877e0033ddb544f68866793eb4e41
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-05-20 19:36:45 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,62 @@
+// Add in the code here to make the server run 
+
+ // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+    //   apiVersion: "2022-08-01",
+    // });
+    
+    
+    // Price ID for product on Stripe Coffee 
+    // price_1N9PgGECwV7seobl06CfD61Z
+
+    const express = require('express');
+    var cors = require('cors');
+    const stripe = require('stripe')('sk_test_51LnUKJDM1jwCEz8OJG69szv032rIo4X0WrFMaXrqxu9g8fdohsL1y54JEUhFUKrqoBquVjN3AzpIFyrbN915bgcd00O5hqoGCJ');
+    
+    const app = express();
+    app.use(cors());
+    app.use(express.static("public"));
+    app.use(express.json());
+    
+    app.post("/checkout", async (req, res) => {
+        /*
+        req.body.items
+        [
+            {
+                id: 1,
+                quantity: 3
+            }
+        ]
+    
+        stripe wants
+        [
+            {
+                price: 1,
+                quantity: 3
+            }
+        ]
+        */
+        console.log(req.body);
+        const items = req.body.items;
+        let lineItems = [];
+        items.forEach((item)=> {
+            lineItems.push(
+                {
+                    price: item.id,
+                    quantity: item.quantity
+                }
+            )
+        });
+    
+        const session = await stripe.checkout.sessions.create({
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel"
+        });
+    
+        res.send(JSON.stringify({
+            url: session.url
+        }));
+    });
+    
+    app.listen(4000, () => console.log("Listening on port 4000!"));
